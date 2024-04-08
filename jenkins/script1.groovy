bd7761cb8cbc78aba57fe14d95f2201c3ab0048f
ajaymaddela <ajaymaddela222@gmail.com>
2024-04-08 07:31:11 +0530

    added jenkins

---
+++ jenkins/script1.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent {label 'nop'}
+    stages{
+        stage(git) {
+            steps {
+              git url: 'https://github.com/nopSolutions/nopCommerce.git',
+                branch:'develop'
+            }
+        }
+        stage(build) {
+            steps {
+              cleanWs()
+              sh 'mkdir published'
+              sh 'dotnet publish -c Release src/Presentation/Nop.Web/Nop.Web.csproj -o ./published'
+              archiveArtifacts artifacts: '**/published/*'
+              junit testResults: '**/surefire-reports/*.xml'
+              
+            }
+        }
+        stage {
+            steps {
+
+            }
+        }
+        
+    }
+}
